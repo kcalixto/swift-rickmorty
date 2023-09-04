b0a6083a8df645e9638a3e55d991b7d90da55d89
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-03 22:07:06 -0300

    added json deserialize introduction + api call

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -10,4 +10,57 @@ import Foundation
 // object to simplify api calls
 final class RMRequest{
     
+//    api call consts
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+//    desired endpoint
+    private let endpoint: RMEndpoint
+//
+    private let pathComponents: [String]
+    private let queryParameters: [URLQueryItem]
+    
+//    construct url for api call
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+
+        if !queryParameters.isEmpty {
+            string += "?"
+                let argumentString = queryParameters.compactMap({
+                    guard let value = $0.value else {return nil}
+                    return "\($0.name)=\(value)"
+                }).joined(separator: "&")
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+//    computed & constructed api url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+//    in this api all requests are get :P
+    public let httpMethod = "GET"
+    
+//    MARK: - Public
+    public init(
+        endpoint: RMEndpoint,
+        pathComponents: [String] = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -12,5 +12,28 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            pathComponents: ["1"],
+            queryParameters: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        print(request.url)
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMCharacter.self
+        ) { result in
+            switch result{
+            case .success:
+                break
+            case .failure(let error):
+                    print(String(describing: error))
+            }
+
+        }
     }
 }
